e8bc28fd829565dcce9bd25f1e435dc9356ec1cf
marcospereira <marcosenpereira@gmail.com>
2021-01-16 08:16:08 -0300

    Auditing data

--- backend/src/main/java/com/bootcampds2/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/resources/CategoryResource.java
@@ -46,6 +46,12 @@ public class CategoryResource {
 		return ResponseEntity.created(uri).body(dto);
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
 		service.delete(id);
