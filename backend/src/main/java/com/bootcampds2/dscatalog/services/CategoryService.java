dbab8e992146f9627eb570a1282f98ef4870c5f2
marcospereira <marcosenpereira@gmail.com>
2021-01-15 22:39:23 -0300

    Delete Category

--- backend/src/main/java/com/bootcampds2/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bootcampds2.dscatalog.dto.CategoryDTO;
 import com.bootcampds2.dscatalog.entities.Category;
 import com.bootcampds2.dscatalog.repositories.CategoryRepository;
+import com.bootcampds2.dscatalog.services.exceptions.DatabaseException;
 import com.bootcampds2.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -51,10 +54,21 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 		}
 		catch(EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id not found "+id);
+			throw new ResourceNotFoundException("Id "  + id +" not found");
 		}
 	}
-	
-	
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id "  + id +" not found");
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
+	}
 
 }
