c8163030f05ea3307b5aefc355361fdbc2be263a
marcospereira <marcosenpereira@gmail.com>
2021-01-20 22:33:39 -0300

    creation of the project and stylization of the navbar

---
+++ front-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './core/assets/styles/custom.scss'
+import './app.scss';
+import Navbar from './core/components/navbar';
+
+
+function App (){
+     return(
+          <h1>
+             <Navbar />
+          </h1>     
+     )
+   
+} 
+
+export default App;
