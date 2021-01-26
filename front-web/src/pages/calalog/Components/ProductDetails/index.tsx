5ebf2f4dc3a5769c81d0fe5daf453b172b2d20a2
marcospereira <marcosenpereira@gmail.com>
2021-01-26 00:03:36 -0300

    Add product listing whit backend

--- front-web/src/pages/calalog/Components/ProductDetails/index.tsx
+++ front-web/src/pages/calalog/Components/ProductDetails/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './styles.scss';
 import {ReactComponent as ArrowIcon} from '../../../../core/assets/Images/arrow.svg'
-import {ReactComponent as ProductImage} from '../../../../core/assets/Images/product.svg'
 import ProductPrice from '../../../../core/components/ProductPrice';
+import { makeRequest } from '../../../../core/utils/request';
+import { Product } from '../../../../core/types/Product';
 
 type ParamsType={
     productId: string;
 }
+
 function ProductDetails(){
     const {productId} = useParams<ParamsType>();
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(()=>{
+        makeRequest ({url: `/products/${productId}`})
+        .then(response=>setProduct(response.data))
+    }, [productId]);
 
     console.log(productId);
     return(
@@ -22,19 +30,17 @@ function ProductDetails(){
                 <div className="row">
                     <div className="col-6 pr-5">
                         <div className="product-details-Image text-center">
-                            <ProductImage />
+                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
                         </div>
-                        <h1 className="product-datails-name">Computador Desktop - Intel Core i7</h1>
-                        <ProductPrice price="2.779,00" />
+                        <h1 className="product-datails-name">
+                            {product?.name}    
+                        </h1>
+                        {product?.price && <ProductPrice price = {product.price} />}
                     </div>
                     <div className="col-6 product-details-card">
                         <h1 className="product-description-title">Descrição do Produto</h1>
                         <p className="product-description-text">
-                            Seja um mestre em multitarefas com a capacidade para exibir quatro 
-                            aplicativos simultâneos na tela. A tela está ficando abarrotada? 
-                            Crie áreas de trabalho virtuais para obter mais espaço e trabalhar 
-                            com os itens que você deseja. Além disso, todas as notificações e 
-                            principais configurações são reunidas em uma única tela de fácil acesso.
+                           {product?.description}
                         </p>
                     </div>
                 </div>
