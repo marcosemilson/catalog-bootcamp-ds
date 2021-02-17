45fdb4e169404fac1391b432b55590629fa55274
marcospereira <marcosenpereira@gmail.com>
2021-02-16 22:54:52 -0300

    Custom Constraintvalidator

--- backend/src/main/java/com/bootcampds2/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,7 @@
 package com.bootcampds2.dscatalog.dto;
 
+import com.bootcampds2.dscatalog.services.validation.UserInsertValid;
+
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
--- backend/src/main/java/com/bootcampds2/dscatalog/services/validation/UserInsertValidator.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/services/validation/UserInsertValidator.java
@@ -6,9 +6,16 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import com.bootcampds2.dscatalog.dto.UserInsertDTO;
+import com.bootcampds2.dscatalog.entities.User;
+import com.bootcampds2.dscatalog.repositories.UserRepository;
+import com.bootcampds2.dscatalog.resources.exceptions.FieldMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
 	
+	@Autowired
+	private UserRepository repository;
 	@Override
 	public void initialize(UserInsertValid ann) {
 	}
@@ -17,9 +24,11 @@ public class UserInsertValidator implements ConstraintValidator<UserInsertValid,
 	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
 		
 		List<FieldMessage> list = new ArrayList<>();
-		
-		// Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
-		
+		User user = repository.findByEmail(dto.getEmail());
+		if(user != null){
+			list.add(new FieldMessage("email", "email já existe"));
+
+		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
