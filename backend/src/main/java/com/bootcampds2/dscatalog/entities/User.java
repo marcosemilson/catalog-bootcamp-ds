07c2acd64e745e7f440f27210a7f4bdb0a9c4960
marcospereira <marcosenpereira@gmail.com>
2021-02-25 22:30:15 -0300

    Implement oauth2 authorization server

--- backend/src/main/java/com/bootcampds2/dscatalog/entities/User.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/entities/User.java
@@ -1,8 +1,9 @@
 package com.bootcampds2.dscatalog.entities;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,27 +16,29 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
-    
+
     @Column(unique = true)
     private String email;
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role",
-        joinColumns = @JoinColumn(name = "user_Id"),
-		inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_Id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
-    
+
     public User() {
     }
 
@@ -87,7 +90,6 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    
     public Set<Role> getRoles() {
         return roles;
     }
@@ -114,6 +116,37 @@ public class User implements Serializable {
                 return false;
         } else if (!id.equals(other.id))
             return false;
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+        .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {        
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {      
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {       
         return true;
     }
 
--- backend/src/main/java/com/bootcampds2/dscatalog/services/UserService.java
+++ backend/src/main/java/com/bootcampds2/dscatalog/services/UserService.java
@@ -15,41 +15,47 @@ import com.bootcampds2.dscatalog.repositories.UserRepository;
 import com.bootcampds2.dscatalog.services.exceptions.DatabaseException;
 import com.bootcampds2.dscatalog.services.exceptions.ResourceNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	private UserRepository repository;
 
 	@Autowired
-	private RoleRepository roleRepository;	
+	private RoleRepository roleRepository;
 
-	
 	@Transactional(readOnly = true)
-	public Page<UserDTO> findAllPaged(PageRequest pageRequest){
+	public Page<UserDTO> findAllPaged(PageRequest pageRequest) {
 		Page<User> list = repository.findAll(pageRequest);
 		return list.map(x -> new UserDTO(x));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		User entity = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+		User entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new UserDTO(entity);
 	}
-	
+
 	@Transactional
 	public UserDTO insert(UserInsertDTO dto) {
 		User entity = new User();
@@ -62,39 +68,48 @@ public class UserService {
 	@Transactional
 	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
-		User entity = repository.getOne(id);
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
-		}
-		catch(EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id "  + id +" not found");
+			User entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
 		}
 	}
 
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
-		}
-		catch(EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id "  + id +" not found");
-		}
-		catch(DataIntegrityViolationException e) {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
-		
+
 	}
+
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setFisrtName(dto.getFirstName());
 		entity.setLastName(dto.getLastName());
 		entity.setEmail(dto.getEmail());
-		
+
 		entity.getRoles().clear();
-		for(RoleDTO roleDTO : dto.getRoles()) {
+		for (RoleDTO roleDTO : dto.getRoles()) {
 			Role role = roleRepository.getOne(roleDTO.getId());
 			entity.getRoles().add(role);
 		}
-		
+
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user==null){
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
 	}
 
 }
