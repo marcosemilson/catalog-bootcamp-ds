15cded90f2711b43a7a9d1e3cacb9bdc30af686c
marcospereira <marcosenpereira@gmail.com>
2021-03-07 09:15:26 -0300

    Finish List Admin Product layout

---
+++ front-web/src/pages/admin/Components/Products/Card/index.tsx
@@ -0,0 +1,36 @@
+import ProductPrice from 'core/components/ProductPrice';
+import React from 'react';
+import './styles.scss';
+
+function Card(){
+    return(
+
+        <div className="card-base product-card-admin">
+            <div className="row">
+                <div className="col-2 text-center border-right py-3">
+                <img 
+                    src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg" 
+                    alt="Produto teste" 
+                    className="product-card-image-admin" />
+                </div>
+                <div className="col-7 py-3">
+                    <h3 className="product-card-name-admin">Computador</h3>
+                    <ProductPrice price ={30.00} />
+                    <div>
+                    <span className="badge rounded-pill bg-secondary">Cat 1</span>
+                    </div>
+                </div>
+                <div className="col-3 pt-3 pr-5">
+                    <button 
+                    type="button" 
+                    className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">Editar</button>
+                     <button 
+                    type="button" 
+                    className="btn btn-outline-danger btn-block border-radius-10 ">Excluir</button>
+                </div>
+            </div>
+        </div>
+    )   
+}
+
+export default Card;
